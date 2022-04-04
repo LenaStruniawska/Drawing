e2ded8ad6d7b485356741b3362452d67cb44f352
Magdalena Struniawska <mag.sok@onet.pl>
2022-04-04 08:17:15 +0200

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Slawek on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
