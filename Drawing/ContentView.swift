106c365cd1bd897896aa394e9694073c8a5c3120
Magdalena Struniawska <mag.sok@onet.pl>
2022-04-07 07:59:44 +0200

    paths

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,10 +7,38 @@
 
 import SwiftUI
 
+
+struct Arc: InsettableShape {
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+    var insetAmount = 0.0
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+    }
+
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(.blue, lineWidth: 40)
+                    
+        
     }
 }
 
