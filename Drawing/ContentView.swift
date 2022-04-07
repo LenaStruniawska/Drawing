3c4515a9f9b04a6346f16b1b6628ade28aaa4a6d
Magdalena Struniawska <mag.sok@onet.pl>
2022-04-07 10:10:22 +0200

    drawingGroup

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,38 +7,50 @@
 
 import SwiftUI
 
-
-struct Arc: InsettableShape {
-    let startAngle: Angle
-    let endAngle: Angle
-    let clockwise: Bool
-    var insetAmount = 0.0
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
     
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-        
-        var path = Path()
-        
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-        
-        return path
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                            color(for: value , brightness: 1),
+                            color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                          ),
+                          lineWidth: 2)
+            }
+        }
+        .drawingGroup()
     }
-
     
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
+
 struct ContentView: View {
+    @State private var colorCicle = 0.0
+    
     var body: some View {
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(.blue, lineWidth: 40)
-                    
-        
+        VStack {
+            ColorCyclingCircle(amount: colorCicle)
+                .frame(width: 300, height: 300)
+            Slider(value: $colorCicle)
+        }
     }
 }
 
